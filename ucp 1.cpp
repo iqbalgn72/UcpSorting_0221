6a1deb58542d24ff7af4d432c58afa6e488d3104
iqbalgn72 <161453083+iqbalgn72@users.noreply.github.com>
2024-03-26 17:04:02 +0700

    Add files via upload

---
+++ ucp 1.cpp
@@ -0,0 +1,23 @@
+
+//1. bekerja dengan cara mengscan list secara berulang membandingkan elemen adjacent dan  menukarnya jika mereka di tempat yg salah
+
+//2. mengscan sera berulang , memilih satu item tiap scan, dan memindahkan item ke posisi yang benar
+
+//3. menggunakan algoritma shell sort karena algoritma ini efisien jika list nya sudah sebagian sudah berurutan 
+
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int
+    string pilihan;
+    while
+    {
+    cout << Set min_index = j
+
+    }
+}
